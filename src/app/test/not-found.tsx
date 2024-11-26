3a05a2174088c04f4b48e2ac0aed4f2c96ec1d7b
INEEDYOUHELP <152955570+INEEDYOUHELP@users.noreply.github.com>
2024-11-27 02:50:45 +0800

    Delete src/app/test directory

--- src/app/test/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
- 
-export default function NotFound() {
-  return (
-    <div className='bg-white p-4 rounded-md shadow-md max-w-md mx-auto text-center mt-20 justify-center items-center'>
-      <h2>局部的not-found</h2 >
-    </div>
-  )
-}
